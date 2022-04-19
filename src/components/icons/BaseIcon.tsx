638918b2f5e4ba26e57a50d46a99673922be2ece
Sulaiman Hamouda <hamouda.sulaiman@gmail.com>
2022-04-18 23:03:06 -0400

    remove fill none

--- src/components/icons/BaseIcon.tsx
+++ src/components/icons/BaseIcon.tsx
@@ -29,17 +29,16 @@ export const BaseIcon: FC<SvgIconProps> = ({
   viewBox,
   children,
   size,
-  stroke,
-  fill,
   className,
   onClick,
+  stroke,
+  fill,
 }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox={viewBox}
     width={size}
     height={size}
-    fill="none"
     onClick={onClick}
     className={`${stroke} ${fill} ${className}`}
   >
